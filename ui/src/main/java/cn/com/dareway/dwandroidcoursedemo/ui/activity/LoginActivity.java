2a602d9ee72818cef3911b967f1555bcd448038d
xhengz <522035487@qq.com>
2016-09-22 09:00:25 +0800

    增加SPDemo

--- ui/src/main/java/cn/com/dareway/dwandroidcoursedemo/ui/activity/LoginActivity.java
+++ ui/src/main/java/cn/com/dareway/dwandroidcoursedemo/ui/activity/LoginActivity.java
@@ -2,10 +2,13 @@ package cn.com.dareway.dwandroidcoursedemo.ui.activity;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import cn.com.dareway.dwandroidcoursedemo.MainActivity;
@@ -16,6 +19,8 @@ public class LoginActivity extends AppCompatActivity {
 
     LinearLayout ll_login;
     Button btn_login;
+    EditText etUsername, etPassword;
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +30,37 @@ public class LoginActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+
+
         ll_login = (LinearLayout) findViewById(R.id.ll_login);
         btn_login = (Button) findViewById(R.id.btn_login);
+        etUsername = (EditText) findViewById(R.id.et_username);
+        etPassword = (EditText) findViewById(R.id.et_password);
+
+
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = sp.getString("username", "");
+        String password = sp.getString("password", "");
+        if (!"".equals(username)) {
+            etUsername.setText(username);
+        }
+
+        if (!"".equals(password)) {
+            etPassword.setText(password);
+        }
 
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //把账户保存到sp里
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("username", etUsername.getText().toString());
+                editor.putString("password", etPassword.getText().toString());
+                editor.apply();
+
+
+                //跳转页面
                 Intent intent = new Intent();
                 intent.setClass(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
